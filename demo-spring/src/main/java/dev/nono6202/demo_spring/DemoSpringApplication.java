733e5b7bea27422041ddbf6d902d7d8d071384ac
NONO6202 <139857693+NONO6202@users.noreply.github.com>
2024-07-09 16:29:18 +0900

    시작이다

---
+++ demo-spring/src/main/java/dev/nono6202/demo_spring/DemoSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.nono6202.demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+}
