2ee76b57e9ec4828ca966a7fdcb68fe7528d5be5
NONO6202 <139857693+NONO6202@users.noreply.github.com>
2024-07-11 18:37:46 +0900

    main화면 꾸미기

--- demo-spring/src/main/java/dev/nono6202/demo_spring/MyController.java
+++ demo-spring/src/main/java/dev/nono6202/demo_spring/MyController.java
@@ -1,15 +1,20 @@
 package dev.nono6202.demo_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class MyController {
 
     @GetMapping("/")
-    public String main(){
+    public String main(Model mo){
+        mo.addAttribute("selector", "main");
         return "main.html";
     }
 
